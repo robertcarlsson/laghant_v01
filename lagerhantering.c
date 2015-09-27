76354143c7e214d93b82eb6ec8380799cd3f11b2
Robert Carlsson <robert.carlsson.5461@student.uu.se>
2015-09-27 20:16:45 +0200

    snart klara

--- lagerhantering.c
+++ lagerhantering.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "goods.c"
+//#include "add_from_stdin.c"
 /*
 void print_good(struct Goods *ware);
 
@@ -14,6 +15,8 @@ void add_good(struct Goods *ware);
 */
 void list_goods(struct Goods *listOfGoods, int index, int page);
 void create_goods(struct Goods *listOfGoods);
+
+int choose_listedgood(struct Goods *listOfGoods, int index);
 //int choose_good(struct Goods *listOfGoods, int index);
 
 
@@ -33,26 +36,28 @@ int main(void)
 	   "[A]dd a good\n"
 	   "[R]emove a good\n"
 	   "[E]dit a good\n"
+	   "[P]rint a good\n"
 	   "[U]ndo a bad decision\n"
 	   "[L]ist everything\n"
 	   "[Q]uit this amazing program\n");
 
-	input = ask_for_char("Your options:  ", "AaRrEeUuLlQq");
+	input = ask_for_char("Your options:  ", "AaRrEePpUuLlQq");
 
 	if      (input == 'A' || input == 'a')
 	  {
-	    //ger oss en peakre till en bit av minne med storleken av struct goods
+	    //ger oss en pekare till en bit av minne med storleken av struct goods
 	    struct Goods *temp = malloc(sizeof(struct Goods));
 	    //checkar så att pekaren inte är null dvs pekaren är giltig
 	    assert(temp != NULL);
 
-      	    add_good(temp, index, saved_wares);
+      	    add_good(temp, index, all_wares);
 
 	    bool done = false;
+	    char choice;
+
 	    while (!done)
 	      {
 		 print_good(temp);
-       	         int choice;
 		 choice = ask_for_char("\nSave, Discard, Edit :", "SsDdEe");
 		 
 		 if      (choice == 'S' || choice == 's')
@@ -89,12 +94,23 @@ int main(void)
 	  }
 	else if (input == 'E' || input == 'e')
 	  {
-	    
-	    
+	    int choosenGood = choose_listedgood(all_wares, index);
+	    if (choosenGood >= 0)
+	      {
+		edit_good(&all_wares[choosenGood]);
+	      }
+	  }
+	else if (input == 'P' || input == 'p')
+	  {
+	    int choosenGood = choose_listedgood(all_wares, index);
+	    if (choosenGood >= 0)
+	      {
+		print_good(&all_wares[choosenGood]);
+	      }
 	  }
 	else if (input == 'U' || input == 'u')
 	  {
-	    if (true)
+	    if (true)//*all_wares == *saved_wares)
 	      {
 		printf("\nNo action to undo: ");
 	      }
@@ -112,8 +128,6 @@ int main(void)
 	else if (input == 'L' || input == 'l')
 	  {
 	    int page = 0;
-	    char *alt1 = "Cc";
-	    char *alt2 = "NnCc";
 	    char choice = 1;
 	    while (choice != 'C' && choice != 'c')
 	      {
@@ -122,11 +136,11 @@ int main(void)
 		if (index >((1+page)*20))
 		  {
 		    
-		    choice = ask_for_char("Next page or Cancel ", alt2);
+		    choice = ask_for_char("\nNext page or Cancel ", "NnCc");
 		  }
 		else 
 		  {
-		    choice = ask_for_char("End page, please Cancel", alt1); 
+		    choice = ask_for_char("\nEnd page, please Cancel", "Cc"); 
 		  }	
 		if (choice == 'N' || choice == 'n')
 		  {
@@ -143,10 +157,42 @@ int main(void)
 }
 
 
+int choose_listedgood(struct Goods *listOfGoods, int index)
+{
+  int page = 0;
+  char choice = 1;
+
+  while (choice != 'C' && choice != 'c')
+    {
+
+      list_goods(listOfGoods, index, page);
+      if (index >((1+page)*20))
+	{	    
+	  choice = ask_for_char("\nChoose ware, Next page or Cancel", "WwNnCc");
+	}
+      else 
+	{
+	  choice = ask_for_char("\nEnd page, Choose ware or Cancel", "WwCc"); 
+	}	
+      if (choice == 'N' || choice == 'n')
+	{
+	  page++;
+	}
+      else if (choice == 'W' || choice == 'w')
+	{
+	  //Returna valet-1 så det blir det correcta indexet
+	  return (ask_for_int("Choose ware: ", 20) -1);	  
+	}
+    }
+  //för att acceptera att man cancelar ifrån den här functionen utan att ange en vara.
+  return -1;
+}
+
+
 void list_goods(struct Goods *listOfGoods, int index, int page)
 {
   for (int i = 0; i < 20 && i < index; i++)
     {
-      printf("\n%d. %s\n", (i+1), listOfGoods[i+(page*20)].name);
+      printf("\n%d. %s", (i+1), listOfGoods[i+(page*20)].name);
     }
 }
